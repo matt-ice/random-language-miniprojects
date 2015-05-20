ead0f6e610301adc7356af7e773f60476ae49884
matt-ice <matus123@gmail.com>
2015-05-20 15:34:17 +0200

    Update and rename python_assemlber to python_assemlber.py
    completed instruction assembly

--- python_assemlber.py
+++ python_assemlber.py
@@ -22,15 +22,26 @@ def parser(instr):
 	else:
 		comp=instr[eq:]
 	
+	#handle integers
+	if(comp in ("0","1","-1")):
+		#begin and handle 0
+		instr_comp[0]="1"
+		instr_comp[2]="1"
+		instr_comp[4]="1"
+		if(comp.find("1")>0):
+			instr_comp[1]="1"
+			if(comp="1"):
+				instr_comp[3]="1"
+				instr_comp[5]="1"
+
+
 	#destination ADM 
 	if(instr.find("A",0,eq)>0):
-		instr_dest[0]="1"
+		instr_dest[0] = "1"
 	if(instr.find("D",0,eq)>0):
 		instr_dest[1] = "1"
 	if(instr.find("M",0,eq)>0):
-		instr_dest= instr_dest + "1"
-	else:
-		instr_dest=instr_dest+"0"
+		instr_dest[2] = "1"
 	
 	# Jump
 	if semicol>0:
@@ -42,8 +53,44 @@ def parser(instr):
 
 	#check if only one input
 	if((comp.find("M")>0 || comp.find("A")>0) != comp.find("D")>0):
+		#prepare instruction part based on AM or D
 		if(comp.find("D")>0):
-			
+			instr_comp[2..3]="11"
+		else:
+			instr_comp[0..1]="11"
+		
+		#!D and !AM
+		if(comp[0]="!"):
+			instr_comp[5]="1"
+		
+		#-AMD and AMD-1
+		if(comp.find("-")>0):
+			instr_comp[4..5]="11"
+			if(comp_find("1")>0):
+				instr_comp[5]="0"
+		#AM+1 and DM+1
+		if(comp.find("+1")>0):
+			instr_comp[1]="1"
+			instr_comp[3..5]="111"
+			if(comp[0]="D"):
+				instr_comp[2]="1"
+			else:
+				instr_comp[0]="1"
+		return
+
+	#handle D+A, D&A, 
+	if(comp[1]="+"):
+		instr_comp[4]="1"
+	elif(comp[1]="&"):
+		return
+
+	#bruteforce the rest
+	if(comp="A-D"):
+		instr_comp="000111"
+	elif(comp="D|A"):
+		instr_comp="010101"
+	elif(comp="D-A"):
+		instr_comp="010011"
 
 
 
@@ -56,13 +103,13 @@ def code:
 
 def main():
 	global instr_type, A_res, instr_dest, instr_comp, instr_jump,instr_a
-	instr_type=""
+	instr_type="C"
 	A_res=""
 	instr_dest="000"
 	instr_comp="000000"
 	instr_jump="000"
 	instr_a="0"
-	load file.asm
+	asm_in = open(file.asm,"r")
 	load output.hack
 	while line.len()<2:
 		line = readline.trim()	
