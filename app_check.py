726ed74d7eb5beb187a1c114d91658421aa7501e
Matúš Karahuta <matus123@gmail.com>
2016-10-28 11:06:06 +0100

    update with periodic saving

--- app_check.py
+++ app_check.py
@@ -7,7 +7,6 @@ file = "H:\classify.xlsx"
 wb = load_workbook(file, data_only=True)
 ws = wb["Sheet1"]
 
-#get first google result
 def get_res(search):
 	url  = "https://www.google.com/search?q=" + str(search).replace(" ","+")
 	req  = urllib.request.Request(url,headers={'User-Agent': 'Mozilla/5.0'})
@@ -16,20 +15,32 @@ def get_res(search):
 	res  = soup.find("div", {"id" : "ires"}).find("h3", {"class" : "r"}).find("a").get_text()
 	return str(res)
 
-#add Play store
 def get_play(s):
 	return get_res(s + "google play")
 
-#add App store
 def get_iOS(s):
 	return get_res(s + "app store")
 
-#start slow
-for i in range(2, 100):
+for i in range(100, 2883):
+#for i in range(31, 40):
 	cell = ws["A"+str(i)].value
-	ws["B"+str(i)] = get_res(cell)
-	ws["C"+str(i)] = get_play(cell)
-	ws["D"+str(i)] = get_iOS(cell)
+	try:
+		ws["B"+str(i)] = get_res(cell)
+	except:
+		ws["B"+str(i)] = "Fail"
+	try:
+		ws["C"+str(i)] = get_play(cell)
+	except:
+		ws["C"+str(i)] = "Fail"
+	try:
+		ws["D"+str(i)] = get_iOS(cell)
+	except:
+		ws["D"+str(i)] = "Fail"
 	if(i % 10 ==0):
-		print("10 passed")
+		print(i + " passed")
+	if(i%500 == 0):
+		wb.save(file)
 wb.save(file)
+
+
+#https://play.google.com/store/search?q=test
